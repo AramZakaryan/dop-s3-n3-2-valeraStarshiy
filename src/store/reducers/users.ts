d0b972d8440254066a296f276d0df60b34947856
AramZakaryan <aram.h.zakaryan@gmail.com>
2023-12-03 13:29:19 +0400

    Final Result (with all corrections with some final touches)

--- src/store/reducers/users.ts
+++ src/store/reducers/users.ts
@@ -13,7 +13,7 @@ export const usersReducer = (state: InitStateType = initState, action: ActionsTy
   switch (action.type) {    
     case 'CREATE_USER':
       return {
-        users: [...state.users, action.payload] as UserType[],
+        users: [...state.users, action.payload],
       };
     default:
       return state;
